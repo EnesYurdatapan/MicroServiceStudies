2fa3cff43bb187d95c247c6b8943d2b1d7143267
Enes Yurdatapan <enesyurdatapan@hotmail.com>
2024-10-30 16:49:03 +0300

    Add project files.

---
+++ MS.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,123 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MS.Services.CouponAPI.Data;
+using MS.Services.CouponAPI.Models;
+using MS.Services.CouponAPI.Models.Dto;
+
+namespace MS.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _responseDto;
+        private IMapper _mapper;
+        public CouponAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _responseDto = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _responseDto.Result= _mapper.Map<IEnumerable<CouponDto>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(u=>u.CouponId==id);
+                _responseDto.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.FirstOrDefault(u => u.CouponCode.ToLower() == code.ToLower());
+               if (coupon == null)
+                    _responseDto.IsSuccess= false;
+                _responseDto.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+        [HttpPost]
+        public ResponseDto Post([FromBody]CouponDto couponDto)
+        {
+            try
+            {
+                _db.Coupons.Add(_mapper.Map<Coupon>(couponDto));
+                _db.SaveChanges();
+                _responseDto.Result = couponDto;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                _db.Coupons.Update(_mapper.Map<Coupon>(couponDto));
+                _db.SaveChanges();
+                _responseDto.Result = couponDto;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+        [HttpDelete]
+        public ResponseDto Put(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.Find(id);
+                _db.Coupons.Remove(coupon);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _responseDto.IsSuccess = false;
+                _responseDto.Message = ex.Message;
+            }
+            return _responseDto;
+        }
+    }
+}
---
+++ MS.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MS.Services.CouponAPI.Models;
+
+namespace MS.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "10FF",
+                DiscountAmount = 10,
+                MinAmount = 20,
+            },
+            new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "20FF",
+                DiscountAmount = 20,
+                MinAmount = 400,
+            }
+            );
+        }
+    }
+}
